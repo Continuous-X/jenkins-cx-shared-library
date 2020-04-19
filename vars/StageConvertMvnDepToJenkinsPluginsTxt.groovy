bc843864361aa7ab45ac3e0866152ba8ace17420
WolverMinion <mikeschmid71@googlemail.com>
2020-04-19 20:00:36 +0200

    fix the stage

--- vars/StageConvertMvnDepToJenkinsPluginsTxt.groovy
+++ vars/StageConvertMvnDepToJenkinsPluginsTxt.groovy
@@ -4,17 +4,15 @@ import com.continuousx.jenkins.pipeline.config.PipelineConfig
 def call(PipelineConfig config) {
 
     stage('Create Jenkins plugins.txt') {
-        steps {
-            def pomXmlFilename = 'pom.xml'
-            def pluginsTxtFilename = 'plugins.txt'
-            def pomXml = readMavenPom file: pomXmlFilename
-            MavenDependenciesToJenkinsPluginsTxt convert = new MavenDependenciesToJenkinsPluginsTxt()
-            String pluginsTxtContent = convert.readPomXmlContent(pomXml)
-                    .and()
-                    .convert2PluginsTxt()
-                    .and().getPluginsTxtContent()
-            writeFile file: pluginsTxtFilename, text: pluginsTxtContent
-            log.info 'build this'
-        }
+        def pomXmlFilename = 'pom.xml'
+        def pluginsTxtFilename = 'plugins.txt'
+        def pomXml = readMavenPom file: pomXmlFilename
+        MavenDependenciesToJenkinsPluginsTxt convert = new MavenDependenciesToJenkinsPluginsTxt()
+        String pluginsTxtContent = convert.readPomXmlContent(pomXml)
+                .and()
+                .convert2PluginsTxt()
+                .and().getPluginsTxtContent()
+        writeFile file: pluginsTxtFilename, text: pluginsTxtContent
+        log.info 'build this'
     }
 }
