68ce8c6ae3a21cf4b77987c74a225203c1862f9d
WolverMinion <mikeschmid71@googlemail.com>
2020-04-27 18:57:52 +0200

    next test

--- src/com/continuousx/jenkins/features/maven/MavenBuildWrapperFeatureImpl.groovy
+++ src/com/continuousx/jenkins/features/maven/MavenBuildWrapperFeatureImpl.groovy
@@ -2,32 +2,27 @@ package com.continuousx.jenkins.features.maven
 
 import com.cloudbees.groovy.cps.NonCPS
 import com.continuousx.jenkins.pipeline.utils.JenkinsPluginCheck
+import groovy.transform.TupleConstructor
 
+@TupleConstructor(includeFields = true)
 class MavenBuildWrapperFeatureImpl implements MavenBuildFeature, Serializable {
     public final static MVN_WRAPPER_FILENAME = 'mvnw'
     public final static MVN_SETTINGS_XML = '.mvn/settings.xml'
 
     private String mvnwCmd = "./${MVN_WRAPPER_FILENAME}"
-    private List<String> necessaryPlugins = [ "workflow-basic-steps", "maven" ]
+    private List<String> neededPlugins = ["workflow-basic-steps", "maven" ]
 
-    private def jenkinsContext
+    def jenkinsContext
 
-
-    MavenBuildWrapperFeatureImpl(jenkinsContext) {
-        Objects.nonNull(jenkinsContext)
-        this.jenkinsContext = jenkinsContext
-
-        assert new JenkinsPluginCheck()
-                .addPluginList(necessaryPlugins)
+    boolean checkNeededPlugins() {
+        return new JenkinsPluginCheck()
+                .addPluginList(neededPlugins)
                 .and()
                 .isPluginListInstalled()
-
-        assert this.jenkinsContext.fileExists(MVN_WRAPPER_FILENAME)
-        assert this.jenkinsContext.fileExists(MVN_SETTINGS_XML)
     }
 
     @NonCPS
-    private boolean isWrapperExist() {
+    boolean isWrapperExist() {
         boolean wrapperFileExist = jenkinsContext.fileExists MVN_WRAPPER_FILENAME
         boolean settingsFileExist = jenkinsContext.fileExists MVN_SETTINGS_XML
 
@@ -40,6 +35,9 @@ class MavenBuildWrapperFeatureImpl implements MavenBuildFeature, Serializable {
 
     @NonCPS
     String startGoal(String goal) {
+        assert isWrapperExist()
+        assert checkNeededPlugins()
+
         return jenkinsContext.sh(
                 script: "${mvnwCmd} ${goal}",
                 returnStdout: true )
@@ -47,6 +45,9 @@ class MavenBuildWrapperFeatureImpl implements MavenBuildFeature, Serializable {
 
     @NonCPS
     String getVersion() {
+        assert isWrapperExist()
+        assert checkNeededPlugins()
+
         return jenkinsContext.sh(
                 script: "${mvnwCmd} --version",
                 returnStdout: true )
