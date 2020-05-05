da0cc620278d54205287cb6be5dbd87af951188f
Wolver Minion <mikeschmid71@googlemail.com>
2020-05-05 19:26:27 +0200

    refactoring

--- vars/StageConvertMvnDepToJenkinsPluginsTxt.groovy
+++ vars/StageConvertMvnDepToJenkinsPluginsTxt.groovy
@@ -1,15 +1,19 @@
 import com.continuousx.jenkins.features.maven.MavenPomFeatureImpl
 import com.continuousx.jenkins.pipeline.config.PipelineConfig
+import com.continuousx.jenkins.pipeline.exceptions.JenkinsPluginNotInstalledException
 
 def call(PipelineConfig config) {
 
     stage('Create Jenkins plugins.txt') {
-        def pomXmlFilename = MavenPomFeatureImpl.POM_XML_FILENAME
-        def pluginsTxtFilename = MavenPomFeatureImpl.PLUGINS_TXT_FILENAME
-        MavenPomFeatureImpl convert = new MavenPomFeatureImpl(this)
-        String pluginsTxtContent = convert.readPomXmlContent()
-                .convertDependencies2PluginsTxt()
-        writeFile file: pluginsTxtFilename, text: pluginsTxtContent
-        log.info "file created: ${pluginsTxtFilename}"
+        try {
+            def pluginsTxtFilename = MavenPomFeatureImpl.PLUGINS_TXT_FILENAME
+            MavenPomFeatureImpl convert = new MavenPomFeatureImpl(this)
+            String pluginsTxtContent = convert.readPomXmlContent()
+                    .convertDependencies2PluginsTxt()
+            writeFile file: pluginsTxtFilename, text: pluginsTxtContent
+            log.info "file created: ${pluginsTxtFilename}"
+        } catch (JenkinsPluginNotInstalledException e) {
+            log.error e.message
+        }
     }
 }
