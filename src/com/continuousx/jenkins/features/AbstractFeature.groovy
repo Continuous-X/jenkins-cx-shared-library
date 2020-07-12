f2959e50fea2e2ba725379e3460622cccdd77dbb
Wolver Minion <mikeschmid71@googlemail.com>
2020-07-12 09:14:42 +0200

    check protection fix

--- src/com/continuousx/jenkins/features/AbstractFeature.groovy
+++ src/com/continuousx/jenkins/features/AbstractFeature.groovy
@@ -6,9 +6,7 @@ import com.continuousx.jenkins.features.metrics.influxdb.InfluxDBFeatureBuilder
 import com.continuousx.jenkins.features.metrics.influxdb.measurements.operating.MeasurementOperatingFeature
 import com.continuousx.utils.github.GHBase
 import com.continuousx.utils.github.GitURLParser
-import com.continuousx.utils.jenkins.JenkinsConfig
 import com.continuousx.utils.jenkins.JenkinsPluginCheck
-import org.kohsuke.github.GHCommitState
 
 abstract class AbstractFeature implements Feature, Serializable{
 
@@ -61,21 +59,29 @@ abstract class AbstractFeature implements Feature, Serializable{
     void runFeature() {
         if(checkNeededPlugins()) {
             try {
+                final long startTime = System.nanoTime()
+/*
                 this.jenkinsContext.withCredentials([this.jenkinsContext.usernamePassword(credentialsId: JenkinsConfig.JENKINS_CONFIG_CREDENTIAL_ID_GITHUB_API, usernameVariable: 'USERNAME', passwordVariable: 'TOKEN')]) {
                     ghBase = new GHBase(this.jenkinsContext.env.GIT_URL, this.jenkinsContext.TOKEN)
                 }
-                final long startTime = System.nanoTime()
                 ghBase.getRepository().createCommitStatus(this.jenkinsContext.env.GIT_COMMIT, GHCommitState.PENDING, this.jenkinsContext.env.GIT_URL, 'my description - pending' )
+*/
                 runFeatureImpl()
+/*
                 ghBase.getRepository().createCommitStatus(this.jenkinsContext.env.GIT_COMMIT, GHCommitState.SUCCESS, this.jenkinsContext.env.GIT_URL, 'my description - success' )
+*/
                 final long duration = (long) ((System.nanoTime() - startTime) / 100000)
                 measurementOperating.setDuration(duration)
             } catch (final Exception exception) {
                 if (this.config.failOnError) {
+/*
                     ghBase.getRepository().createCommitStatus(this.jenkinsContext.env.GIT_COMMIT, GHCommitState.ERROR, this.jenkinsContext.env.GIT_URL, 'my description - error' )
+*/
                     throw exception
                 } else {
+/*
                     ghBase.getRepository().createCommitStatus(this.jenkinsContext.env.GIT_COMMIT, GHCommitState.FAILURE, this.jenkinsContext.env.GIT_URL, 'my description - failure' )
+*/
                     jenkinsContext.log.warning("${this.config.type} failed: ${exception.message}")
                 }
             } finally {
@@ -88,7 +94,6 @@ abstract class AbstractFeature implements Feature, Serializable{
     }
 
     @Override
-    @NonCPS
     void publishMetricOperating() {
         metrics.publishMetricOperating(measurementOperating)
     }
