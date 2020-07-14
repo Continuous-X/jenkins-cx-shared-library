ca1ef93eb8073d9b670de186853612624c1a2e10
Wolver Minion <mikeschmid71@googlemail.com>
2020-07-14 21:08:51 +0200

    test pipeline

--- src/com/continuousx/jenkins/pipelines/Pipeline.groovy
+++ src/com/continuousx/jenkins/pipelines/Pipeline.groovy
@@ -2,9 +2,6 @@ package com.continuousx.jenkins.pipelines
 
 interface Pipeline {
 
-    List<String> neededPlugins
-    PipelineConfig pipelineConfig
-
     void publishMetricOperating()
 
 }
--- src/com/continuousx/jenkins/stages/Stage.groovy
+++ src/com/continuousx/jenkins/stages/Stage.groovy
@@ -2,9 +2,6 @@ package com.continuousx.jenkins.stages
 
 interface Stage {
 
-    List<String> neededPlugins
-    StageConfig stageConfig
-
     void runStage()
     void publishMetricOperating()
 
