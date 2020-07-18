9c62c28707829517a635ca83bc1f55122b97daaf
Wolver Minion <mikeschmid71@googlemail.com>
2020-07-18 19:17:52 +0200

    next manual tests

--- src/com/continuousx/utils/rulecheck/ghprotection/GHBranchProtectionCheck.groovy
+++ src/com/continuousx/utils/rulecheck/ghprotection/GHBranchProtectionCheck.groovy
@@ -1,6 +1,7 @@
 package com.continuousx.utils.rulecheck.ghprotection
 
 import com.cloudbees.groovy.cps.NonCPS
+import com.continuousx.jenkins.logger.PipelineLogger
 import com.continuousx.utils.rulecheck.RuleBook
 import com.continuousx.utils.rulecheck.RuleBookEntry
 import com.continuousx.utils.rulecheck.RuleChecker
@@ -13,9 +14,10 @@ class GHBranchProtectionCheck implements RuleChecker {
 
     @Override
     @NonCPS
-    boolean checkRules(RuleSet ruleSet, final Object checkedObject) {
+    boolean checkRules(RuleSet ruleSet, final Object checkedObject, final PipelineLogger logger) {
         Objects.requireNonNull(ruleSet)
         Objects.requireNonNull(checkedObject)
+        logger.logInfo"use ruleset '${ruleSet.getClass().getName()}' and check '${checkedObject.getClass().getName()}'"
         assert checkedObject instanceof GHBranch
         final GHBranch branch = checkedObject as GHBranch
 
@@ -24,10 +26,16 @@ class GHBranchProtectionCheck implements RuleChecker {
             ruleBookEntry.rule = rule
             ruleBookEntry.passed = rule.check(branch)
             ruleBookEntry.note = ruleBookEntry.passed ? rule.successfulNote : rule.condition
+            logger.logDebug """
+rulebook entry
+rule: ${ruleBookEntry.rule.toString()}
+passed: ${ruleBookEntry.passed}
+note: ${ruleBookEntry.note}
+"""
             ruleBook.addEntry ruleBookEntry
         }
         final List<RuleBookEntry> resultList = ruleBook.getEntryList().findAll { entry -> entry.passed == false }
-        println "found passes: ${resultList.size()}"
+        logger.logInfo "found passes: ${resultList.size()}"
         resultList.size() == 0
     }
 
