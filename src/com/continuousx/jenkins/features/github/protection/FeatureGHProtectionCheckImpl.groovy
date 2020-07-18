4c97f7ab9827b84df7073d6dae7fcfe3475b9e55
Wolver Minion <mikeschmid71@googlemail.com>
2020-07-18 19:58:32 +0200

    next manual tests

--- src/com/continuousx/jenkins/features/github/protection/FeatureGHProtectionCheckImpl.groovy
+++ src/com/continuousx/jenkins/features/github/protection/FeatureGHProtectionCheckImpl.groovy
@@ -1,9 +1,11 @@
 package com.continuousx.jenkins.features.github.protection
 
 import com.continuousx.jenkins.features.AbstractFeature
+import com.continuousx.utils.github.GHBase
 import com.continuousx.utils.jenkins.JenkinsConfig
 import com.continuousx.utils.rulecheck.ghprotection.GHBranchProtectionCheck
 import com.continuousx.utils.rulecheck.ghprotection.RuleSetProtectionSimple
+import org.kohsuke.github.GHBranch
 
 class FeatureGHProtectionCheckImpl extends AbstractFeature {
 
@@ -19,7 +21,8 @@ class FeatureGHProtectionCheckImpl extends AbstractFeature {
     void runFeatureImpl() {
         logger.logInfo "start Protection check on '${this.jenkinsContext.env.GIT_BRANCH}'"
         this.jenkinsContext.withCredentials([this.jenkinsContext.usernamePassword(credentialsId: JenkinsConfig.JENKINS_CONFIG_CREDENTIAL_ID_GITHUB_API, usernameVariable: 'USERNAME', passwordVariable: 'TOKEN')]) {
-            final boolean protectionResult = new GHBranchProtectionCheck().checkRules(new RuleSetProtectionSimple(), this.jenkinsContext.env.GIT_BRANCH, logger)
+            final GHBranch branch = GHBase.getRepositoryBranch(this.jenkinsContext.env.GIT_BRANCH as String, this.jenkinsContext.env.GIT_URL as String, GHBase.getConnetctionOAuth(this.jenkinsContext.TOKEN as String))
+            final boolean protectionResult = new GHBranchProtectionCheck().checkRules(new RuleSetProtectionSimple(), branch, logger)
             logger.logInfo "Protection on '${this.jenkinsContext.env.GIT_BRANCH}': ${protectionResult}"
         }
     }
