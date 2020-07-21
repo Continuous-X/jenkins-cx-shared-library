05a5139524caf9fb8c866eeeac21c189af9371cf
Wolver Minion <mikeschmid71@googlemail.com>
2020-07-21 22:03:42 +0200

    next tests

--- src/com/continuousx/utils/jenkins/JenkinsEnv.groovy
+++ src/com/continuousx/utils/jenkins/JenkinsEnv.groovy
@@ -1,7 +1,13 @@
 package com.continuousx.utils.jenkins
 
+import com.cloudbees.groovy.cps.NonCPS
+
 enum JenkinsEnv {
 
     CX_SHARED_LIB_BRANCH
 
+    @Override
+    String toString() {
+        name()
+    }
 }
