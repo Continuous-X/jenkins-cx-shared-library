879cb7bae1e3bc0611a862b179f63d945ab17472
Wolver Minion <mikeschmid71@googlemail.com>
2020-07-21 19:03:01 +0200

    next tests

--- src/com/continuousx/jenkins/features/jenkins/globallib/FeatureJenkinsGlobalLibVersionImpl.groovy
+++ src/com/continuousx/jenkins/features/jenkins/globallib/FeatureJenkinsGlobalLibVersionImpl.groovy
@@ -26,7 +26,7 @@ class FeatureJenkinsGlobalLibVersionImpl extends AbstractFeature {
         List<LibraryConfiguration> libraryConfigurationList = globalLibraries.getLibraries()
 
         LibraryConfiguration libraryConfiguration = libraryConfigurationList.find {it.name == JenkinsConfig.JENKINS_CONFIG_GLOBAL_LIBRARY_JENKINS_CX_SHARED_LIB}
-        logger.logInfo "libConfig: ${libraryConfiguration} "
+        logger.logInfo "libConfig: ${libraryConfiguration.getDefaultVersion()} "
         logger.logInfo "${JenkinsConfig.JENKINS_CONFIG_GLOBAL_LIBRARY_JENKINS_CX_SHARED_LIB}:  "
         logger.logInfo "${JenkinsConfig.JENKINS_CONFIG_GLOBAL_LIBRARY_JENKINS_CX_SHARED_LIB}:  ${this.jenkinsContext.env.library.${JenkinsConfig.JENKINS_CONFIG_GLOBAL_LIBRARY_JENKINS_CX_SHARED_LIB}.version}"
     }
