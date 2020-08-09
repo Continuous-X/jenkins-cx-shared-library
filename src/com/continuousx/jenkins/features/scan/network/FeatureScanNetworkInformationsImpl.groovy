be20e77009f56c8e016d2f49a956e19f270e7701
Wolver Minion <mikeschmid71@googlemail.com>
2020-08-09 19:29:37 +0200

    scan network

--- src/com/continuousx/jenkins/features/scan/network/FeatureScanNetworkInformationsImpl.groovy
+++ src/com/continuousx/jenkins/features/scan/network/FeatureScanNetworkInformationsImpl.groovy
@@ -20,6 +20,23 @@ class FeatureScanNetworkInformationsImpl extends AbstractFeature {
         logger.logInfo "IP: ${InetAddress.getLocalHost()}"
         logger.logInfo "IP(4): ${Inet4Address.getLocalHost()}"
         logger.logInfo "IP(6): ${Inet6Address.getLocalHost()}"
+        NetworkInterface.getNetworkInterfaces().each {networkInterface ->
+            logger.logInfo 'list inet adresses'
+            networkInterface.getInetAddresses().each {inetAdresses ->
+                logger.logInfo "adress: ${inetAdresses.getHostAddress()} / name: ${inetAdresses.getHostName()}"
+            }
+            logger.logInfo 'list interface adresses'
+            networkInterface.getInterfaceAddresses().each {interfaceAdresses ->
+                logger.logInfo "adress: ${interfaceAdresses.getAddress().toString()} / broadcast: ${interfaceAdresses.getBroadcast().toString()}"
+            }
+            logger.logInfo 'list sub network interfaces'
+            networkInterface.getSubInterfaces().each {subInterfaces ->
+                logger.logInfo "\tsub network interface: ${subInterfaces.getDisplayName()}"
+                subInterfaces.getInetAddresses().each {subInetAdresses ->
+                    logger.logInfo "adress: ${subInetAdresses.getHostAddress()} / name: ${subInetAdresses.getHostName()}"
+                }
+            }
+        }
     }
 
 }
