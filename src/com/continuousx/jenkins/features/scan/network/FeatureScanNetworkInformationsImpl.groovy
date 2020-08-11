ec60d63d6d98948e22f8197b053ddabd5a6324c7
Wolver Minion <mikeschmid71@googlemail.com>
2020-08-11 22:07:32 +0200

    scan network

--- src/com/continuousx/jenkins/features/scan/network/FeatureScanNetworkInformationsImpl.groovy
+++ src/com/continuousx/jenkins/features/scan/network/FeatureScanNetworkInformationsImpl.groovy
@@ -26,8 +26,8 @@ class FeatureScanNetworkInformationsImpl extends AbstractFeature {
             StringBuilder formatedMac = new StringBuilder()
             if (readedMac != null) {
                 readedMac.encodeHex().toString().toCharArray().eachWithIndex { myChar, index ->
-                    index.mod(2) == 0 && index < 11 ? formatedMac.append(myChar) : formatedMac.append(myChar).append('-')
-                    logger.logInfo "index (${index}/${myChar}/${index.mod(2) == 0}/${index < 11}): ${formatedMac}"
+                    index.mod(2) == 0 && index < readedMac.encodeHex().toString().size() ? formatedMac.append(myChar) : formatedMac.append(myChar).append('-')
+                    logger.logInfo "index (${index}/${myChar}/${index.mod(2) == 0}/${index < readedMac.encodeHex().toString().size()}): ${formatedMac}"
                 }
             }
             logger.logInfo "list inet adresses (${networkInterface.getInetAddresses().toList().size()}): ${readedMac} / ${formatedMac}"
